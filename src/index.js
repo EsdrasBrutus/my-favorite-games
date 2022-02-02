a5ea34f1803d88698037513e07e90360514b2569
GenHero <genhero@hotmail.com>
2022-02-02 16:47:55 -0500

    setup redux store and initial colors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './state';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,5 +19,5 @@ function App() {
       App
     </div>
   );
-}
+};
 
