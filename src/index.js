abb173c70fb323688f34c8ed7ad237cb7f7d632e
GenHero <genhero@hotmail.com>
2022-02-02 18:32:32 -0500

    intial setup for routes

--- src/index.js
+++ src/index.js
@@ -2,21 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './state';
+import {
+  BrowserRouter as Router,
+  Route,
+  useHistory,
+  Routes,
+} from 'react-router-dom';
+
+import Homepage from './components/pages/Home/Homepage';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <Router>
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  </Router>,
   document.getElementById('root')
 );
 
 function App() {
   return (
     <div className="App">
-      App
+      <Routes>
+        <Route exact path="/" element={<Homepage/>} />
+      
+      </Routes>
     </div>
   );
 };
